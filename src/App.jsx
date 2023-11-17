6fd62814a03d22066de2d8bf034774e3b3b377bf
Yohannes-Asfaw <jonathanasfaw11@gmail.com>
2023-11-17 11:18:17 +0300

    whole project commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { TicTacToe } from './components/TicTacToe/TicTacToe'
+ function App(){
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+export default App
